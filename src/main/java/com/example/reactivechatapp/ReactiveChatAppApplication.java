d36acf92fef2e12b99e7f368c1365e3aae2dcabd
saul <dlathf3210@naver.com>
2023-04-25 14:47:53 +0900

    first commit

---
+++ src/main/java/com/example/reactivechatapp/ReactiveChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivechatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveChatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reactivechatapp/ReactiveChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivechatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
